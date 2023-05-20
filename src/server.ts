9495e4f4ca2d8dff3936c987e407f11e2592de6e
Leandro Parice <contato@leandroparice.com.br>
2023-05-20 19:17:04 -0300

    aula 3

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import 'dotenv/config'
 
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -11,6 +13,11 @@ app.register(cors, {
   // origin: ['http://localhost:3000', 'http://rocketseat.com.br/']
 })
 
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
