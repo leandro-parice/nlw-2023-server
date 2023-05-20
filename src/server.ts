a0a3da8bc8545a8b5c04f8c46074575d89937bb7
Leandro Parice <contato@leandroparice.com.br>
2023-05-20 16:57:24 -0300

    aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-  return users
+app.register(cors, {
+  origin: true,
+  // origin: ['http://localhost:3000', 'http://rocketseat.com.br/']
 })
+
+app.register(memoriesRoutes)
 
 app
   .listen({
